b731a004016501514aee85a025191b3b92508392
Sebastián Torres <seba.torresz@duocuc.cl>
2023-12-06 23:28:50 -0300

    se añaden rutas en ítems

--- src/assets/componentes/ProductCard.jsx
+++ src/assets/componentes/ProductCard.jsx
@@ -1,19 +1,19 @@
-// src/ProductCard.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-    const ProductCard = ({ product }) => {
-    const { category, name, price } = product;
+const ProductCard = ({ product }) => {
+    const { id, category, name, price } = product;
 
     return (
-        <div className="card" style={{ width: '18rem' }}>
-        <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{category}</h6>
-            <p className="card-text">Precio: ${price}</p>
-            <a href="#" className="btn btn-primary">
-            Ver Detalles
-            </a>
-        </div>
+        <div className="card mb-4">
+            <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text">Category: {category}</p>
+                <p className="card-text">Price: ${price}</p>
+                <Link to={`/detalle/${category.toLowerCase()}/${id}`} className="btn btn-primary">
+                    Ver Detalles
+                </Link>
+            </div>
         </div>
     );
 };
--- src/assets/componentes/ProductCatalog.jsx
+++ src/assets/componentes/ProductCatalog.jsx
@@ -1,4 +1,3 @@
-// src/ProductCatalog.js
 import React from 'react';
 import ProductCard from './ProductCard';
 
